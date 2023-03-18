a6960abd5f5d97c0f5bd82fe70d7415d61ad1243
jonasqin <137141449@qq.com>
2023-03-18 14:02:24 +0800

    202303181402

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -30,7 +30,7 @@ module.exports = {
       },
       items: [
         {
-          href: 'https://twitter.com/jokenomicser/',
+          href: 'https://twitter.com/jojonas_xyz/',
           label: 'Twitter',
           position: 'right',
         },
